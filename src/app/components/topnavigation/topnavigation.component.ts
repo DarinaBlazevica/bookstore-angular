00bb2709d8895c2061bd700783366f4d456deb1b
Darina <darina.blazevica@accenture.com>
2022-05-13 11:53:25 +0300

    top-nav

--- src/app/components/topnavigation/topnavigation.component.ts
+++ src/app/components/topnavigation/topnavigation.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-topnavigation',
   templateUrl: './topnavigation.component.html',
   styleUrls: ['./topnavigation.component.css']
 })
+
 export class TopnavigationComponent implements OnInit {
 
-  constructor() { }
+  @Input() bookstore_name: string;
+  @Input() cart: any;
+  @Input() hamburger: any;
 
+  constructor() {
+    this.bookstore_name =''
+    this.cart = ''
+    this.hamburger=''
+  }
+      
   ngOnInit(): void {
   }
 
